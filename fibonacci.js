99dbba6e8ec5b2948681105501920605c8eaad96
prio12 <maksudurprio.mp@gmail.com>
2025-09-13 13:15:59 +0600

    solved returning Nth Fibonacci number problem

--- fibonacci.js
+++ fibonacci.js
@@ -31,3 +31,35 @@ function fibonacci(n) {
 
 // * Time Complexity: O(n)
 // * Space Complexity: O(n)
+
+//! Given a number n, return the Nth Fibonacci number (0-indexed).
+
+//? Input: 0
+//? Output: 0
+
+function nthFibonacci(n) {
+  const seq = [0, 1];
+
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  for (let i = 2; i < n; i++) {
+    seq[i] = seq[i - 1] + seq[i - 2];
+  }
+
+  return seq[n - 1];
+}
+
+//* Steps to solve
+//* (1) Start with [0,1]
+//* (2) directly return if the n is 0, 1
+//* (3) otherwise make the seq array first
+//* (4) then return seq [n -1]
+
+//? to visualize
+// *n = 5
+// *seq = [0, 1, 1, 2, 3]
+// *Nth Fibonacci = seq[5-1] = 3
+
+// * Time Complexity: O(n)
+// * Space Complexity: O(n)

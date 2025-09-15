54bce5d65cfb50a5d1d501cb379b257ddc532e57
prio12 <maksudurprio.mp@gmail.com>
2025-09-15 15:48:57 +0600

    solved Problem: Sum of Factorial Digits

--- factorial.js
+++ factorial.js
@@ -1,6 +1,6 @@
 //find the factorial of an integer
 
-//! Given a number n, find the factorial of it.
+//! 01 Given a number n, find the factorial of it.
 
 //*Explanation
 
@@ -17,10 +17,68 @@ function factorial(n) {
 }
 
 // * Test cases
-// console.log(factorial(0)); //0
+// console.log(factorial(10));
 // console.log(factorial(1)); //1
 // console.log(factorial(2)); //2
 // console.log(factorial(3)); //6
 
 // * Time Complexity: O(n)
 // * Space Complexity: O(1)
+
+//! 02 Problem: Trailing Zeros in Factorial
+
+//* Explanation:
+// *Given a number `n`, find how many trailing zeros are there in `n!`.
+// *Trailing zeros are zeros at the end of the factorial number.
+
+//* Example:
+// ? Input: 5
+// ? Output: 1
+// * Explanation: 5! = 120 → 1 trailing zero
+
+// ? Input: 10
+// ? Output: 2
+// * Explanation: 10! = 3628800 → 2 trailing zeros
+
+// * Task:
+// Write a function `trailingZeros(n)` that returns the count of trailing zeros in n!.
+
+function trailingZeros(n) {
+  let count = 0;
+  for (let i = 5; i <= n; i *= 5) {
+    count += Math.floor(n / i);
+  }
+  return count;
+}
+
+// * Time Complexity: O(n)
+// * Space Complexity: O(1)
+
+// console.log(trailingZeros(24), 'trailing');
+
+//! 03 Problem: Sum of Factorial Digits
+//*Description: Given a number n, calculate n! (factorial of n) and then find the sum of all its digits.
+//?Example:
+
+//?Input: 5 → 5! = 120 → sum of digits = 1 + 2 + 0 = 3
+
+//?Input: 3 → 3! = 6 → sum of digits = 6
+
+function sum(n) {
+  let factorial = 1;
+  let total = 0;
+  for (let i = n; i > 0; i--) {
+    factorial *= i;
+  }
+  const numberString = factorial.toString();
+
+  for (let i = 0; i < numberString.length; i++) {
+    total += parseInt(numberString[i]);
+  }
+  return total;
+}
+
+// * Time Complexity: O(n^2)
+// * Space Complexity: O(1)
+
+console.log(sum(10));

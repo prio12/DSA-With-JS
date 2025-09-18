c43db19ec0f8b123faf00e2aabb72f75bdc5daa9
prio12 <maksudurprio.mp@gmail.com>
2025-09-18 17:05:59 +0600

    solved Give a positive integer n, determine if the number is a power of 2 or not

---
+++ powerOfTwo.js
@@ -0,0 +1,18 @@
+//! (1) Give a positive integer "n", determine if the number is a power of 2 or not
+
+function isPowerOfTwo(n) {
+  if (n < 1) return false;
+
+  while (n > 1) {
+    if (n % 2 !== 0) return false;
+    n = n / 2;
+  }
+
+  return true;
+}
+
+console.log(isPowerOfTwo(1));
+
+//*notes
+//*keep dividing till the number (n) becomes 1
+//* for loop can be used here too but while loop is appropriate for this particular problem

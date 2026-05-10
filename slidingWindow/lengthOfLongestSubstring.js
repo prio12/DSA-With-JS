39a821b6b4505b3f0b5c4f8b418f4cf66254a04a
prio12 <maksudurprio.mp@gmail.com>
2026-05-10 12:25:35 +0600

    solved: length of longest substring

---
+++ slidingWindow/lengthOfLongestSubstring.js
@@ -0,0 +1,20 @@
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0;
+  let longest = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    longest = Math.max(longest, right - left + 1);
+  }
+
+  return longest;
+}
+
+console.log(lengthOfLongestSubstring('abcabcbb')); // 3
+console.log(lengthOfLongestSubstring('bbbbb')); // 1
+console.log(lengthOfLongestSubstring('pwwkew')); // 3

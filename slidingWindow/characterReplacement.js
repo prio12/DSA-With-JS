ae33b2862d2cb4051aee7ed90f65a9691ff1977a
prio12 <maksudurprio.mp@gmail.com>
2026-05-13 22:07:53 +0600

    refactor: moved characterReplacement solution to slidingWindow folder

--- slidingWindow/characterReplacement.js
+++ slidingWindow/characterReplacement.js
@@ -1,22 +1,23 @@
 function characterReplacement(s, k) {
   let map = {};
   let left = 0;
-  let maxLength = 0;
-
+  let maxWindowSize = 0;
   for (let right = 0; right < s.length; right++) {
     if (map[s[right]]) {
       map[s[right]]++;
     } else {
       map[s[right]] = 1;
     }
+
     while (right - left + 1 - Math.max(...Object.values(map)) > k) {
-      map[s[left]] = map[s[left]] - 1;
+      map[s[left]]--;
       left++;
     }
-    maxLength = Math.max(maxLength, right - left + 1);
+
+    maxWindowSize = Math.max(maxWindowSize, right - left + 1);
   }
 
-  return maxLength;
+  return maxWindowSize;
 }
 
-console.log(characterReplacement('AABABBA', 1));
+console.log(characterReplacement('ABAA', 0));
